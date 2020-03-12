9a14c2c6f5f8fc2701ac3be4a989e1462bf3a254
Jake Itegboje <jakeitegsy@yahoo.com>
2020-03-11 20:02:27 -0500

    Create $stack.py

---
+++ $stack.py
@@ -0,0 +1,9 @@
+from aws_cdk.core import Stack, Construct
+
+
+class $STACK(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+        
+        # Constructs
